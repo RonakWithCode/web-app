56d78d1037cc68b7add67f4be396b6e0504c78e8
akashJainAJ11 <akashjain100200@gmail.com>
2024-06-02 15:06:04 +0530

    home update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from '../Components/HeroSection'
+import { WeAre } from '../Components/WeAre'
 
 const Home = ()=> {
   return (
     <div>
       <HeroSection/>
-    </>
+      <WeAre/>
+    </div>
   )
 }
 
